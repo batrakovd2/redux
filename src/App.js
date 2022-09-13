93996c8d9e0a17f7ea0173ac05cedf7797d2bfe1
batrakovd2 <batrakovd2@gmail.com>
2022-09-13 12:54:16 +0500

    combineReducer

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import {useDispatch, useSelector} from "react-redux";
 
 function App() {
   const dispatch = useDispatch()
-  const cash = useSelector(state => state.cash)
+  const cash = useSelector(state => state.cashReducer.cash)
+
+  const addCash = (cash) => {
+    dispatch({type: 'ADD_CASH', payload: cash})
+  }
+
+  const getCash = (cash) => {
+    dispatch({type: 'GET_CASH', payload: cash})
+  }
+
   console.log(cash)
   return (
     <div className="App">
       <div style={{display:'flex'}}>
         <div>{cash}</div>
-        <button>Пополнить счет</button>
-        <button>Снять со счета</button>
+        <button onClick={() => addCash( Number(prompt()) )}>Пополнить счет</button>
+        <button onClick={() => getCash( Number(prompt()) )}>Снять со счета</button>
       </div>
     </div>
   );

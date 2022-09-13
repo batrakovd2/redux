6fbb2b7dee71f0f31d4d229bf6dbca621ad15523
batrakovd2 <batrakovd2@gmail.com>
2022-09-13 09:02:03 +0500

    useSelector

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+  console.log(cash)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{display:'flex'}}>
+        <div>{cash}</div>
+        <button>Пополнить счет</button>
+        <button>Снять со счета</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 const defaultState = {
   cash: 0
@@ -20,13 +21,15 @@ const reducer = (state = defaultState, action) => {
   }
 }
 
-const store = createStore();
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store} >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
